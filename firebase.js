ca477f7d1ca7e3fb6abd455c9ce694211aa91659
akito-jibiki <aj@akito-jibikinoMacBook-Pro.local>
2022-11-28 18:30:21 +0900

    install next-auth and firebase and initialize the firebase

---
+++ firebase.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp, getApp, getApps } from "firebase/app";
+import {getFirestore} from "firebase/firestore";
+import {getStorage} from "firebase/storagefirestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "slipper-v2.firebaseapp.com",
+  projectId: "slipper-v2",
+  storageBucket: "slipper-v2.appspot.com",
+  messagingSenderId: "183574700949",
+  appId: "1:183574700949:web:258d5d22923c3410d3cc9c"
+};
+
+// Initialize Firebase
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore();
+const storage = getStorage();
+export {app, db,storage,};

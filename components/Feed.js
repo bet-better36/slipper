58d59de887679b98ac44b97ac588d7731ed53330
akito-jibiki <aj@akito-jibikinoMacBook-Pro.local>
2022-12-02 20:56:47 +0900

    get post data from firestore and show them in the posts sction

--- components/Feed.js
+++ components/Feed.js
@@ -1,30 +1,23 @@
 import { SparklesIcon } from "@heroicons/react/outline"
+import { collection, onSnapshot, orderBy, query } from "firebase/firestore"
+import { useEffect, useState } from "react"
+import { db } from "../firebase"
 import Input from "./Input"
 import Post from "./Post"
 
 export default function Feed() {
-  const posts = [
-    {
-      id: "1",
-      name: "fdsfds",
-      username: "fdsfds",
-      userImg: "/images/slipper-logo.png",
-      img: "https://images.unsplash.com/photo-1523712999610-f77fbcfc3843?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-      timestamp: "5h ago",
+  const [posts, setPosts] = useState([]);
+  useEffect(
+    () =>
+      onSnapshot(
+        query(collection(db, "posts"), orderBy("timestamp", "desc")),
+        (snapshot) => {
+          setPosts(snapshot.docs);
+        }
+      ),
+    []
+  );
 
-    },
-
-    {
-      id: "2",
-      name: "fdsfds",
-      username: "fdsfds",
-      userImg: "/images/slipper-logo.png",
-      img: "https://images.unsplash.com/photo-1548248823-ce16a73b6d49?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1588&q=80",
-      text: "foea",
-      timestamp: "5h ago",
-
-    }
-  ]
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
         <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
--- components/Post.js
+++ components/Post.js
@@ -1,11 +1,11 @@
 import { ChartBarIcon, ChatIcon, DotsHorizontalIcon, HeartIcon, ShareIcon, TrashIcon } from "@heroicons/react/outline"
-
+import Moment from "react-moment"
 
 export default function Post({post}) {
   return (
     <div className="flex p-3 cursol-pointer border-b border-gray-200">
         {/* {user img} */}
-        <img className="h-11 w-11 rounded-full mr-4" src={post.userImg} alt="user-img" />
+        <img className="h-11 w-11 rounded-full mr-4" src={post.data().userImg} alt="user-img" />
 
         {/* right side */}
         <div className="">
@@ -14,9 +14,11 @@ export default function Post({post}) {
         <div className="flex items-center justify-between">
             {/* post user info */}
             <div className="flex items-center space-x-1 whitespace-nowrap">
-                <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.name}</h4>
-                <span className="text-sm sm:text-[15px]">@{post.username} - </span>
-                <span className="text-sm sm:text-[15px] hover:underline">{post.timestamp}</span>
+                <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.data().name}</h4>
+                <span className="text-sm sm:text-[15px]">@{post.data().username} - </span>
+                <span className="text-sm sm:text-[15px] hover:underline">
+                  <Moment fromNow>{post.timestamp?.toDate()}</Moment>
+                </span>
 
             </div>
 
@@ -25,10 +27,10 @@ export default function Post({post}) {
         </div>
 
         {/* post text */}
-        <p className="text-gray-800 text-[15px sm:text-[16px] mb-2]">{post.text}</p>
+        <p className="text-gray-800 text-[15px sm:text-[16px] mb-2]">{post.data().text}</p>
 
         {/* post image */}
-        <img className="rounded-2xl mr-2" src={post.img} alt="bgfh" />
+        <img className="rounded-2xl mr-2" src={post.data().image} alt="bgfh" />
 
         {/* icons */}
         <div className="flex justify-between text-gray-500 p-2">

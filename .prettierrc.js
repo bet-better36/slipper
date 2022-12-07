239c55fa6134efe2adee00a867c22d5144381d42
akito-jibiki <aj@akito-jibikinoMacBook-Pro.local>
2022-12-08 04:08:23 +0900

    get the comments from firestore and create  the comment component

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,6 +1,6 @@
 {
   module.exports = {
-    printWidth: 100,
+    printWidth: 80,
     tabWidth: 2,
     useTabs: false,
     semi: true,
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,15 +6,29 @@ import Sidebar from '../../components/Sidebar';
 import Widgets from '../../components/Widgets';
 import Post from '../../components/Post';
 import { db } from '../../firebase';
-import { doc, onSnapshot } from 'firebase/firestore';
+import { collection, doc, onSnapshot, orderBy, query } from 'firebase/firestore';
 import { useEffect, useState } from 'react';
+import Comment from '../../components/Comment';
 
 export default function PostPage({ newsResults, randomUsersResults }) {
   const router = useRouter();
   const { id } = router.query;
   const [post, setPost] = useState();
+  const [comments, setComments] = useState([]);
+
+  //get the post data
 
   useEffect(() => onSnapshot(doc(db, 'posts', id), (snapshot) => setPost(snapshot)), [db, id]);
+
+  //get comments of the post
+
+  useEffect(() => {
+    onSnapshot(
+      query(collection(db, 'posts', id, 'comments'), orderBy('timestamp', 'desc')),
+      (snapshot) => setComments(snapshot.docs),
+    );
+  }, [db, id]);
+
   return (
     <div>
       <Head>
@@ -36,7 +50,16 @@ export default function PostPage({ newsResults, randomUsersResults }) {
             </div>
             <h2 className='text-lg sm:text-xl font-bold cursol-pointer'>Tweet</h2>
           </div>
+
           <Post id={id} post={post} />
+
+          {comments.length > 0 && (
+            <div className=''>
+              {comments.map((comment) => (
+                <Comment key={comment.id} id={comment.id} comment={comment.data()} />
+              ))}
+            </div>
+          )}
         </div>
 
         {/* {widgets} */}

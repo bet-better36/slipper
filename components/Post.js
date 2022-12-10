63814dea4dfb8d04132606300701b798ef26b9e2
akito-jibiki <aj@akito-jibikinoMacBook-Pro.local>
2022-12-10 17:06:36 +0900

    complete the comment component

--- components/Post.js
+++ components/Post.js
@@ -5,17 +5,23 @@ import {
   HeartIcon,
   ShareIcon,
   TrashIcon,
-} from '@heroicons/react/outline';
-import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid';
-import { collection, deleteDoc, doc, onSnapshot, setDoc } from 'firebase/firestore';
-import Moment from 'react-moment';
-import { db, storage } from '../firebase';
-import { signIn, useSession } from 'next-auth/react';
-import { useEffect, useState } from 'react';
-import { deleteObject, ref } from 'firebase/storage';
-import { modalState, postIdState } from '../atom/modalAtom';
-import { useRecoilState } from 'recoil';
-import { useRouter } from 'next/router';
+} from "@heroicons/react/outline";
+import { HeartIcon as HeartIconFilled } from "@heroicons/react/solid";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  onSnapshot,
+  setDoc,
+} from "firebase/firestore";
+import Moment from "react-moment";
+import { db, storage } from "../firebase";
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from "react";
+import { deleteObject, ref } from "firebase/storage";
+import { modalState, postIdState } from "../atom/modalAtom";
+import { useRecoilState } from "recoil";
+import { useRouter } from "next/router";
 
 export default function Post({ post, id }) {
   const { data: session } = useSession();
@@ -27,27 +33,31 @@ export default function Post({ post, id }) {
   const router = useRouter();
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) =>
-      setLikes(snapshot.docs),
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", id, "likes"),
+      (snapshot) => setLikes(snapshot.docs),
     );
   }, [db]);
 
   useEffect(() => {
-    const unsubscribe = onSnapshot(collection(db, 'posts', id, 'comments'), (snapshot) =>
-      setComments(snapshot.docs),
+    const unsubscribe = onSnapshot(
+      collection(db, "posts", id, "comments"),
+      (snapshot) => setComments(snapshot.docs),
     );
   }, [db]);
 
   useEffect(() => {
-    setHasLiked(likes.findIndex((like) => like.id === session?.user.uid) !== -1);
+    setHasLiked(
+      likes.findIndex((like) => like.id === session?.user.uid) !== -1,
+    );
   }, [likes]);
 
   async function likePost() {
     if (session) {
       if (hasLiked) {
-        await deleteDoc(doc(db, 'posts', id, 'likes', session?.user.uid));
+        await deleteDoc(doc(db, "posts", id, "likes", session?.user.uid));
       } else {
-        await setDoc(doc(db, 'posts', id, 'likes', session?.user.uid), {
+        await setDoc(doc(db, "posts", id, "likes", session?.user.uid), {
           username: session.user.username,
         });
       }
@@ -58,42 +68,59 @@ export default function Post({ post, id }) {
 
   async function deletePost() {
     if (window.confirm(`この投稿を削除しますか？`)) {
-      deleteDoc(doc(db, 'posts', id));
+      deleteDoc(doc(db, "posts", id));
       if (post.data().image) {
         deleteObject(ref(storage, `post/${id}/image`));
       }
-      router.push('/');
+      router.push("/");
     }
   }
 
   return (
-    <div className='flex p-3 cursol-pointer border-b border-gray-200'>
+    <div className="flex p-3 cursol-pointer border-b border-gray-200">
       {/* {user img} */}
-      <img className='h-11 w-11 rounded-full mr-4' src={post?.data()?.userImg} alt='user-img' />
+      <img
+        className="h-11 w-11 rounded-full mr-4"
+        src={post?.data()?.userImg}
+        alt="user-img"
+      />
       {/* right side */}
-      <div className='flex-1'>
+
+      <div className="flex-1">
         {/* header */}
-        <div className='flex items-center justify-between'>
+        <div className="flex items-center justify-between">
           {/* post user info */}
-          <div className='flex items-center space-x-1 whitespace-nowrap'>
-            <h4 className='font-bold text-[15px] sm:text-[16px] hover:underline'>
+          <div className="flex items-center space-x-1 whitespace-nowrap">
+            <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">
               {post?.data()?.name}
             </h4>
-            <span className='text-sm sm:text-[15px]'>@{post?.data()?.username} -</span>
-            <span className='text-sm sm:text-[15px] hover:underline'>
+            <span className="text-sm sm:text-[15px]">
+              @{post?.data()?.username} -
+            </span>
+            <span className="text-sm sm:text-[15px] hover:underline">
               <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment>
             </span>
           </div>
           {/* dot icon */}
-          <DotsHorizontalIcon className='h-10 hoverEffect w-10 hover:text-sky-500 p-2' />
+          <DotsHorizontalIcon className="h-10 hoverEffect w-10 hover:text-sky-500 p-2" />
         </div>
         {/* post text */}
-        <p className='text-gray-800 text-[15px] sm:text-[16px] mb-2'>{post?.data()?.text}</p>
+        <p
+          onClick={() => router.push(`/posts/${id}`)}
+          className="text-gray-800 text-[15px] sm:text-[16px] mb-2"
+        >
+          {post?.data()?.text}
+        </p>
         {/* post image */}
-        <img className='rounded-2xl mr-2' src={post?.data()?.image} alt='' />
+        <img
+          onClick={() => router.push(`/posts/${id}`)}
+          className="rounded-2xl mr-2"
+          src={post?.data()?.image}
+          alt=""
+        />
         {/* icons */}
-        <div className='flex justify-between text-gray-500 p-2'>
-          <div className='flex items-center select-none'>
+        <div className="flex justify-between text-gray-500 p-2">
+          <div className="flex items-center select-none">
             <ChatIcon
               onClick={() => {
                 if (!session) {
@@ -103,38 +130,42 @@ export default function Post({ post, id }) {
                   setOpen(!open);
                 }
               }}
-              className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100'
+              className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100"
             />
-            {comments.length > 0 && <span className='text-sm'>{comments.length}</span>}
+            {comments.length > 0 && (
+              <span className="text-sm">{comments.length}</span>
+            )}
           </div>
           {session?.user.uid === post?.data()?.id && (
             <TrashIcon
               onClick={deletePost}
-              className='h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100'
+              className="h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100"
             />
           )}
-          <div className='flex items-center'>
+          <div className="flex items-center">
             {hasLiked ? (
               <HeartIconFilled
                 onClick={likePost}
-                className='h-9 w-9 hoverEffect p-2 text-red-500 hover:bg-red-100'
+                className="h-9 w-9 hoverEffect p-2 text-red-500 hover:bg-red-100"
               />
             ) : (
               <HeartIcon
                 onClick={likePost}
-                className='h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100'
+                className="h-9 w-9 hoverEffect p-2 hover:text-red-500 hover:bg-red-100"
               />
             )}
             {likes.length > 0 && (
-              <span className={`${hasLiked && 'text-red-600'} text-sm select-none`}>
+              <span
+                className={`${hasLiked && "text-red-600"} text-sm select-none`}
+              >
                 {likes.length}
               </span>
             )}
           </div>
-          <ShareIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
-          <ChartBarIcon className='h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100' />
+          <ShareIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
+          <ChartBarIcon className="h-9 w-9 hoverEffect p-2 hover:text-sky-500 hover:bg-sky-100" />
         </div>
-        <div className=''></div>
+        <div className=""></div>
       </div>
     </div>
   );
